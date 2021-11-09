cc90757ab7d3e6ded432894a343fb546a1c578bd
mansibabbar <mansib@carbyne911.com>
2021-11-09 20:56:26 +0530

    Added tests of user and department services

--- department-service/src/test/java/com/dailycodebuffer/department/DepartmentServiceApplicationTests.java
+++ department-service/src/test/java/com/dailycodebuffer/department/DepartmentServiceApplicationTests.java
@@ -1,13 +1,38 @@
 package com.dailycodebuffer.department;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class DepartmentServiceApplicationTests {
+	String cloudGatewayURL = "http://localhost:9191";
 
 	@Test
-	void contextLoads() {
+	public void testPostDepartment() throws IOException {
+		HttpPost postRequest = new HttpPost(cloudGatewayURL + "/departments/");
+		String departmentJson = "{\"departmentName\": \"Test\",\"departmentAddress\": \"TestAddress\", \"departmentCode\": \"TestCode\"}";
+		StringEntity departmentEntity = new StringEntity(departmentJson, ContentType.APPLICATION_JSON);
+		postRequest.setEntity(departmentEntity);
+		HttpResponse httpPostResponse = HttpClientBuilder.create().build().execute(postRequest);
+		assertThat(httpPostResponse.getStatusLine().getStatusCode()).isEqualTo(HttpStatus.SC_OK);
 	}
 
+	@Test
+	public void testGetDepartment() throws IOException {
+		int departmentId = 1;
+		HttpGet getRequest = new HttpGet(cloudGatewayURL + "/departments/" + departmentId);
+		HttpResponse httpGetResponse = HttpClientBuilder.create().build().execute(getRequest);
+		assertThat(httpGetResponse.getStatusLine().getStatusCode()).isEqualTo(HttpStatus.SC_OK);
+	}
 }
--- user-service/src/test/java/com/dailycodebuffer/user/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/dailycodebuffer/user/UserServiceApplicationTests.java
@@ -1,13 +1,38 @@
 package com.dailycodebuffer.user;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class UserServiceApplicationTests {
+	String cloudGatewayURL = "http://localhost:9191";
 
 	@Test
-	void contextLoads() {
+	public void testPostUser() throws IOException {
+		HttpPost postRequest = new HttpPost(cloudGatewayURL + "/users/");
+		String userJson = "{\"firstName\": \"TestUserName\",\"lastName\": \"TestUserLastName\",\"email\": \"TestUserEmail\",\"departmentId\": 1}";
+		StringEntity userEntity = new StringEntity(userJson, ContentType.APPLICATION_JSON);
+		postRequest.setEntity(userEntity);
+		HttpResponse httpPostResponse = HttpClientBuilder.create().build().execute(postRequest);
+		assertThat(httpPostResponse.getStatusLine().getStatusCode()).isEqualTo(HttpStatus.SC_OK);
 	}
 
+	@Test
+	public void testGetUser() throws IOException {
+		int userId = 1;
+		HttpGet getUserRequest = new HttpGet(cloudGatewayURL + "/users/" + userId);
+		HttpResponse httpGetResponse = HttpClientBuilder.create().build().execute(getUserRequest);
+		assertThat(httpGetResponse.getStatusLine().getStatusCode()).isEqualTo(HttpStatus.SC_OK);
+	}
 }
