3418dc4a9ea2ead4990c07bafa1e277cfabe8c8d
mansibabbar <mansib@carbyne911.com>
2021-11-09 21:34:05 +0530

    Added object mapper for user

--- user-service/src/test/java/com/dailycodebuffer/user/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/dailycodebuffer/user/UserServiceApplicationTests.java
@@ -1,5 +1,8 @@
 package com.dailycodebuffer.user;
 
+import com.dailycodebuffer.user.VO.Department;
+import com.dailycodebuffer.user.entity.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpGet;
@@ -17,12 +20,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class UserServiceApplicationTests {
 	String cloudGatewayURL = "http://localhost:9191";
+	private final ObjectMapper mapper = new ObjectMapper();
 
 	@Test
 	public void testPostUser() throws IOException {
 		HttpPost postRequest = new HttpPost(cloudGatewayURL + "/users/");
-		String userJson = "{\"firstName\": \"TestUserName\",\"lastName\": \"TestUserLastName\",\"email\": \"TestUserEmail\",\"departmentId\": 1}";
-		StringEntity userEntity = new StringEntity(userJson, ContentType.APPLICATION_JSON);
+		User user = new User((long)1001, "Test UserFirstName", "Test UserLastName", "testUserEmail@gmail.com", (long)1);
+		StringEntity userEntity = new StringEntity(mapper.writeValueAsString(user), ContentType.APPLICATION_JSON);
 		postRequest.setEntity(userEntity);
 		HttpResponse httpPostResponse = HttpClientBuilder.create().build().execute(postRequest);
 		assertThat(httpPostResponse.getStatusLine().getStatusCode()).isEqualTo(HttpStatus.SC_OK);
@@ -30,7 +34,19 @@ class UserServiceApplicationTests {
 
 	@Test
 	public void testGetUser() throws IOException {
-		int userId = 1;
+		HttpPost postDepartmentRequest = new HttpPost(cloudGatewayURL + "/departments/");
+		Department department = new Department((long)3, "Test Department", "Test Department Address", "Test Department Code");
+		StringEntity departmentEntity = new StringEntity(mapper.writeValueAsString(department), ContentType.APPLICATION_JSON);
+		postDepartmentRequest.setEntity(departmentEntity);
+		HttpClientBuilder.create().build().execute(postDepartmentRequest);
+
+		HttpPost postUserRequest = new HttpPost(cloudGatewayURL + "/users/");
+		User user = new User((long)1002, "Test UserFirstName", "Test UserLastName", "testUserEmail@gmail.com", (long)3);
+		StringEntity userEntity = new StringEntity(mapper.writeValueAsString(user), ContentType.APPLICATION_JSON);
+		postUserRequest.setEntity(userEntity);
+		HttpClientBuilder.create().build().execute(postUserRequest);
+
+		int userId = 1002;
 		HttpGet getUserRequest = new HttpGet(cloudGatewayURL + "/users/" + userId);
 		HttpResponse httpGetResponse = HttpClientBuilder.create().build().execute(getUserRequest);
 		assertThat(httpGetResponse.getStatusLine().getStatusCode()).isEqualTo(HttpStatus.SC_OK);
